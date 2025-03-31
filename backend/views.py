ff273240867a80b81b553a2ef7098160c405cccd
azamat1012 <148556590+azamat1012@users.noreply.github.com>
2025-03-31 17:09:39 +0500

    Update views.py
    -Исправил quiz_result (да, второй раз)  на рендеринг инфы релевантно - wedding -> свадьба -Теперь определяет заранее есть ли цветы в диапазонах цен или нет и в соответствии с этим показывает интерфейс

--- backend/views.py
+++ backend/views.py
@@ -39,10 +39,12 @@ def home(request):
     first_row = bouquets[:3]
     return render(request, 'index.html', {'first_row': first_row})
 
+
 class BouquetDetailView(DetailView):
     model = Bouquet
     template_name = 'card.html'
     context_object_name = 'bouquet'
+
 
 def create_order(request, ):
 
@@ -54,7 +56,6 @@ def create_order(request, ):
         bouquet_id = request.POST.get('bouquet_id')
         bouquet = get_object_or_404(Bouquet, id=bouquet_id)
 
-
         Order.objects.create(
             customer_name=name,
             customer_phone=phone,
@@ -64,9 +65,10 @@ def create_order(request, ):
 
         )
         request.session['bouquet_id'] = bouquet_id
-        return redirect('payment' )
+        return redirect('payment')
     bouquet_id = request.GET.get('bouquet_id')
     return render(request, 'order.html', {'bouquet_id': bouquet_id})
+
 
 def get_catalog(request):
     bouquets = list(Bouquet.objects.all())
@@ -92,6 +94,7 @@ def get_consultation(request):
 
     return render(request, 'consultation.html')
 
+
 def get_quiz_first(request):
     if request.method == 'POST':
         event = request.POST.get('event')
@@ -114,16 +117,28 @@ def quiz_results(request):
     event = request.session.get('event')
     budget = request.session.get('budget')
     bouquets = Bouquet.objects.all()
+    event_display = {
+        "wedding": 'Свадьба',
+        "no_occasion": 'Без повода',
+        "birthday": 'День рождения',
+
+    }.get(event, event)
+    price_range = None
     if budget:
         price_range = PriceRange.objects.filter(name=budget).first()
         if price_range:
             if price_range.min_price is not None:
-                bouquets = bouquets.filter(total_price__gte=price_range.min_price)
+                bouquets = bouquets.filter(
+                    total_price__gte=price_range.min_price)
             if price_range.max_price is not None:
-                bouquets = bouquets.filter(total_price__lte=price_range.max_price)
-
+                bouquets = bouquets.filter(
+                    total_price__lte=price_range.max_price)
+        else:
+            bouquets.objects.none()
+    is_there_any_fower = bouquets.exists()
     return render(request, 'result.html', {
-        'event': event,
+        'is_there_any_flower': is_there_any_fower,
+        'event': event_display,
         'budget': budget,
-        'bouquets': bouquets
+        'bouquets': bouquets,
     })
